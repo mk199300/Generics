915276d06706448f4537abf1d64de52ffefda2de
michal <ksm199300@gmail.com>
2024-05-20 15:28:52 +0300

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        public static void PrintTheObj<T>(T obj)
+        {
+            Console.WriteLine(obj);
+        }
+
+        static void Main(string[] args)
+        {
+            PrintTheObj<int>(42);
+            PrintTheObj<string>("hello");
+            PrintTheObj<double>(1.2);
+
+            SwapList<int> list = new SwapList<int>();
+            list.Add(42);
+            list.Add(30);
+            list.Add(20);
+
+            Console.WriteLine("1" + " " + list[0] + " " + "2" + " " + list[1] + " " + "3" + " " + list[2]);
+            list.Swap(1,2);
+            Console.WriteLine("1" + " " + list[0] + " " + "2" + " " + list[1] + " " + "3" + " " + list[2]);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Generics/SwapList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class SwapList<T> : List<T>
+    {
+        public List<T> list { set; get; }
+        public SwapList() { }
+        public void  Swap ( int index1,int index2)
+        {
+
+            T help = this.list[index1];
+            this.list[index1]= this.list[index2];
+            this.list[index2] = help;
+
+        }
+
+    }
+
+}
